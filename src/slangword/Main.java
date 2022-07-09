82dcc914eb3d24d24152558434767af00743b227
Tridev24 <tridev2410@gmail.com>
2022-07-09 17:12:30 +0700

    Add Search slangword feature

---
+++ src/slangword/Main.java
@@ -0,0 +1,162 @@
+package slangword;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author bin
+ */
+public class Main {
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SlangWordApplication app = new SlangWordApplication();
+        boolean isLoaded = app.loadData("slang.txt");
+        if(isLoaded) {
+            app.run();
+        }
+    }
+}
+
+class SlangWordApplication{
+    SlangWordList slangWordList = new SlangWordList();
+    LinkedHashMap history = new LinkedHashMap<String, String>();
+     
+    public boolean loadData(String fileName){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            System.out.println("reader: " + reader);
+            String line = reader.readLine();
+            
+            while(line != null) {
+                String[] tokens = line.split("`");
+                slangWordList.add(tokens[0], tokens[1]);
+                line = reader.readLine();
+            }
+            
+            reader.close();
+            
+            return true;
+        } catch(IOException e) {
+        }
+        
+        return false;
+    }
+  
+    public File createFileIfNotExist(String fileName) {
+        File file = new File(fileName);
+        try {
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+    
+    
+    public void run() {
+        int id = 0;
+        do {
+            clearScreen();
+            System.out.println("\t---Welcome to Slang Word List Application---");
+            System.out.println("1. Search by slang word\t\t\t6. Delete slang word");
+            System.out.println("2. Search by definition\t\t\t7. Restore backup");
+            System.out.println("3. View search history\t\t\t8. On this day slang word");
+            System.out.println("4. Add new slang word\t\t\t9. Random game: guess the definition");
+            System.out.println("5. Edit slang word\t\t\t10. Random game: guess the slang word");
+            System.out.println("0. Exit");
+            System.out.println("\t---------------------------------------------");
+            if(id<0||id>10) {
+                System.out.print("Invalid choice! Please enter your choice again: ");
+            }
+            else
+            {
+                System.out.print("Enter your choice: ");
+            }
+
+            BufferedReader bReader;
+            try {
+                bReader = new BufferedReader(new InputStreamReader(System.in, "utf-8"));
+                String choice = bReader.readLine();
+                if(isNumeric(choice)) {
+                    id = Integer.parseInt(choice);
+                }
+                else{
+                    id= - 1;
+                }
+                switch (id) {
+                    case 1:
+                        clearScreen();
+                        SearchSlangWord();
+                        break;
+                    
+                    case 0:
+                        clearScreen();                        
+                        System.out.println("Thank you for using Slang Word List Application!");
+                        // saveData("slang.txt");
+                        System.exit(0);
+                    default:
+                        break;
+                }
+        } catch (  IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        } while (id<0||id>5);
+    } 
+     
+    private void SearchSlangWord() {
+        clearScreen();
+        System.out.println("Search a slang word");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+        
+        if(slangWordList.slangWords.get(word) != null) {
+            String value =  slangWordList.slangWords.get(word);
+            System.out.println("Definition: " + value);
+            history.put(word, value);
+        }
+        else {
+            System.out.println("Not found!");
+        }
+        System.out.println("Press Enter to continue...");
+        scanner.nextLine();
+        run();
+    }
+
+    
+    private boolean isNumeric(String readLine) {
+        return readLine.matches("[0-9]+");
+    }
+    
+    public static void clearScreen() {  
+       //Clears Screen in java
+        try {
+        if (System.getProperty("os.name").contains("Windows"))
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        else
+            Runtime.getRuntime().exec("clear");
+     } catch (IOException | InterruptedException ex) {}
+    }
+}
---
+++ src/slangword/SlangWordList.java
@@ -0,0 +1,52 @@
+package slangword;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bin
+ */
+public class SlangWordList {
+    HashMap<String, String> slangWords;
+    
+    public SlangWordList() {
+        slangWords = new HashMap<String, String>();
+    }
+    
+    public void add(String word, String definition) {
+        if(!slangWords.containsKey(word)) {
+            slangWords.put(word, definition);
+        }
+    }
+    
+    public int size() {
+        return slangWords.size();
+    }
+
+    public String get(String word) {
+        return slangWords.get(word);
+    }
+
+    public void remove(String word) {
+        slangWords.remove(word);
+    }
+
+    public SlangWordList search(String definition) {
+        SlangWordList result = new SlangWordList();
+        for (String word : slangWords.keySet()) {
+            System.out.println(word);
+            // TODO: Refactor this. 
+            if (Pattern.compile(Pattern.quote(definition),Pattern.CASE_INSENSITIVE).matcher(slangWords.get(word)).find()) {
+                result.add(word, slangWords.get(word));
+            }
+        }
+        return result;
+    }
+}
